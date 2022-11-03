ffc03d09715a4060177e90d46f176b4b7f0c7527
Roosevelt Blow <rooseveltblow@gmail.com>
2022-11-03 15:58:05 -0600

    completes express router

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
+
 const logger = require('./middleware/logger');
 
 const app = express();
@@ -15,14 +15,13 @@ app.use(logger);
 //     response.sendFile(path.join(__dirname, 'public','index.html'));
 // })
 
-//Gets All Members
-app.get('/api/members', (req, res) => {
-    res.json(members);
-})
+
 
 //SET STATIC FOLDER
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
