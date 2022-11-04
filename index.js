bd6935fda1501d3ee153dedb67bb09b63896c80e
Roosevelt Blow <rooseveltblow@gmail.com>
2022-11-04 11:16:49 -0600

    completes code along

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const Members = require('./Members');
 
 const logger = require('./middleware/logger');
 
@@ -10,13 +12,25 @@ const app = express();
 //Init middleware
 //app.use(logger);
 
+//HANDLEBARS MIDDLEWARE
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//HOMEPAGE ROUTE
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Member App',
+        Members
+    });
+})
+
+//SET STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
+
 //BODY PARSER MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
-//SET STATIC FOLDER
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 //API MEMBERS ROUTES
 app.use('/api/members', require('./routes/api/members'));
--- routes/api/members.js
+++ routes/api/members.js
@@ -33,7 +33,8 @@ router.post('/', (req,res) => {
         return res.status(400).json({msg: 'Please include a name and email'});
     }
     members.push(newMember);
-    res.json(members);
+    // res.json(members);
+    res.redirect('/');
 })
 
 //UPDATE MEMBER
